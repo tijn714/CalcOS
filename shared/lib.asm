7b669caad31a3b2515e303d882f7653c07719fc5
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2023-04-12 09:47:20 +0200

    Delete shared directory

--- shared/lib.asm
+++
@@ -1,7 +0,0 @@
-clear_screen:
-    mov ah, 0x06      ; function 0x06: scroll window up
-    mov al, 0x00      ; clear entire screen
-    mov cx, 0x00      ; upper left corner row and column
-    mov dx, 0x184F    ; lower right corner row and column (80x25 text mode)
-    int 0x10
-    ret
